07f16b54ee69294c21c228cad0ea4d9fc9feed8a
dcwalk <dcwalk@users.noreply.github.com>
2017-02-19 10:46:12 -0500

    fix typo in issues header

--- learn/issues.php
+++ learn/issues.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Contribute | IXmaps</title>
+    <title>Issues | IXmaps</title>
     <?php include '../_includes/global-head.php'; ?>
 </head>
 
