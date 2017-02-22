cd9f3609b9fd32b4016d4474ecd02df661242c90
Colin <colindmccann@gmail.com>
2017-02-22 12:36:11 +0700

    closes #13

--- _includes/global-footer.php
+++ _includes/global-footer.php
@@ -34,7 +34,7 @@
         <div class="footer-copyright">
             <p class="minor">The IXmaps website is licensed under a
                 <a href="https://creativecommons.org/licenses/by-sa/4.0" target="_blank">Creative Commons Share-Alike License</a><br>
-                Last modified Tuesday 21 February, 2016
+                Last modified recently (site under active development)
             </p>
 
             <a id="privacy" class="ui transparent button labeled icon" href="privacy.php" target="_self"><i class="icon"><img src="/_assets/img/icn-privacypolicy.svg" alt=""></i>Privacy Policy</a>
