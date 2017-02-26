637f3f0ebf36af615a1c736f925b6bf12749e940
dcwalk <dcwalk@users.noreply.github.com>
2017-02-26 14:28:16 -0500

    move images to src dir and clean up assets

--- _assets/__build/Gruntfile.js
+++ _assets/__build/Gruntfile.js
@@ -151,7 +151,7 @@ module.exports = function( grunt ) {
 				files: [{
 					expand: true,
 					cwd: '<%= dirs.src.images %>/',
-					src: ['**'],
+					src: ['**/*.{png,jpg,gif}'],
 					dest: '<%= dirs.images %>/'
 				}]
 			}
