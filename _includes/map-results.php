371e8119fac0a1b8c5ae6f1dfcb13ed6722dc8d0
agamba <lodeanto@gmail.com>
2017-03-14 22:09:33 -0400

    map results leyend

--- _includes/map-results.php
+++ _includes/map-results.php
@@ -1,8 +1,10 @@
 <div class="results">
     <header class="header-results">
-        <p><strong><span id="tot-results-found"></span></strong> Traceroutes Found <br />
-            <em>Displaying <span id="tr-count"></span> of <span id="tot-results"></span> most recent search results</em>
-        </p>
+        <div>
+            <strong><span id="tot-results-found"></span></strong> Traceroutes Found <br />
+            <em>Displaying <span id="tr-count"></span> of a sample of <span id="tot-results"></span> search results</em>
+            <!-- most recent search results -->
+        </div>
         <div id="filter-results-summary-container">Search results details...</div>
         <div id="filter-results-summary" class="hidden"></div>
     </header><!-- /header -->
