482eb380f43ea17fab7dff0c9789e71ad94db49c
Colin <colindmccann@gmail.com>
2017-03-17 13:45:20 +0700

    ok, fixed issue with url_base

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -23,7 +23,7 @@ var loadConfig = function(configUrl) {
         console.log('Config loaded');
 
         //var url_base = location.origin; // Get URI dymanically
-        var url_base = config.php_backend; // Use URI from config file
+        url_base = config.php_backend; // Use URI from config file
       },
       error: function(xhr, code, error) {
         console.error("Couldn't load `"+configUrl+"`: ", code, error, xhr);
