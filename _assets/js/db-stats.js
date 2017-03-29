ec8fa42ecfbcd2f77595aefae08bcdc8e0949d0a
Colin <colindmccann@gmail.com>
2017-03-29 22:42:07 +0700

    switching to non-dynamic db stats

--- _assets/js/db-stats.js
+++ _assets/js/db-stats.js
@@ -1,22 +1,31 @@
 var getDbStats = function() {
   console.log("Loading getDbStats");
 
-  var obj = {
-    action: 'getDbStats'
-  };
+  // var obj = {
+  //   action: 'getDbStats'
+  // };
 
-  jQuery.ajax(url_base + '/application/controller/db_stats.php', {
-    type: 'post',
-    data: obj,
-    success: function (e) {
-      console.log("Ok! getDbStats");
-      var data = jQuery.parseJSON(e);
-      renderDbStats(data);
-    },
-    error: function (e) {
-      console.log("Error! getDbStats", e);
-    }
-  });
+  // jQuery.ajax(url_base + '/application/controller/db_stats.php', {
+  //   type: 'post',
+  //   data: obj,
+  //   success: function (e) {
+  //     console.log("Ok! getDbStats");
+  //     var data = jQuery.parseJSON(e);
+  //     renderDbStats(data);
+  //   },
+  //   error: function (e) {
+  //     console.log("Error! getDbStats", e);
+  //   }
+  // });
+
+  // TODO: change me back!
+  var data = {
+    "submitters": 700,
+    "destinations": 4545,
+    "traceroutes": 181876,
+    "latest_contribution": "29 Mar 2017"
+  }
+  renderDbStats(data);
 };
 
 var renderDbStats = function(data) {
