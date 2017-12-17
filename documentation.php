d8d29e3b9c5163cfb6362aed3f35911d6ce05e68
Colin <colindmccann@gmail.com>
2017-12-17 15:21:14 +0700

    added pdf for documentation

---
+++ documentation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Documentation | IXmaps</title>
+</head>
+
+<body id="documentation-page">
+
+<!--   <header class="hero">
+      <div class="content">
+          <h1>Documentation</h1>
+      </div>
+  </header> -->
+
+  <div class="content">
+      <embed src="docs/stack-overview.pdf"
+          width="1000"
+          height="780"
+          showcontrols="false"
+          type='application/pdf' />
+  </div>
+
+</body>
+</html>
