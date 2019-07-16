1dfcb04fe471c5c5e6bfe5fbdcab1f1f1baab2f9
Colin <colindmccann@gmail.com>
2019-07-15 20:05:29 -0400

    adding production config flag

--- _includes/global-head.php
+++ _includes/global-head.php
@@ -26,25 +26,28 @@
 <script src="/_assets/__build/bower_components/jquery-toastmessage-plugin/src/main/javascript/jquery.toastmessage.js"></script>
 <script src="/_assets/js/main.js"></script>
 
-<!-- Piwik -->
 <script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["setDomains", ["*.www.ixmaps.ca"]]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//www.ixmaps.ca/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
+  
 </script>
-<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
 
-
+<!-- Piwik -->
 <script type="text/javascript">
   loadConfig('/config.json')
   verifyConfig(config, requiredConfig);
+
+  if (config.env == "prod") {
+    var _paq = _paq || [];
+    _paq.push(["setDomains", ["*.www.ixmaps.ca"]]);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//www.ixmaps.ca/piwik/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 1]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  }
 </script>
+<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
