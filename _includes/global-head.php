e86b7a10fcab1629f4f4123ae2a71caa2e57e335
Colin <colindmccann@gmail.com>
2019-10-01 14:16:47 +0200

    updates for new matomo

--- _includes/global-head.php
+++ _includes/global-head.php
@@ -30,7 +30,7 @@
   
 </script>
 
-<!-- Piwik -->
+<!-- Matomo -->
 <script type="text/javascript">
   loadConfig('/config.json')
   verifyConfig(config, requiredConfig);
@@ -43,11 +43,11 @@
     (function() {
       var u="//www.ixmaps.ca/piwik/";
       _paq.push(['setTrackerUrl', u+'piwik.php']);
-      _paq.push(['setSiteId', 1]);
+      _paq.push(['setSiteId', 5]);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
   }
 </script>
-<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
+<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
+<!-- End Matomo Code -->
