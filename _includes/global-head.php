23de4cf7a6896d94dcfe3e76021ebc35929febf0
Colin <colindmccann@gmail.com>
2019-10-16 18:56:34 +0200

    resetting site id for matomo

--- _includes/global-head.php
+++ _includes/global-head.php
@@ -43,11 +43,11 @@
     (function() {
       var u="//www.ixmaps.ca/piwik/";
       _paq.push(['setTrackerUrl', u+'piwik.php']);
-      _paq.push(['setSiteId', 5]);
+      _paq.push(['setSiteId', 1]);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
   }
 </script>
-<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="//www.ixmaps.ca/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Matomo Code -->
