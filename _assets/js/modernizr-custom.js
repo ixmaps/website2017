f937311e73bde3f39eb0612657b62f87e3978933
agamba <lodeanto@gmail.com>
2019-10-16 12:52:51 -0400

    cleaning up on the server, figure out a better way to do this asap

--- _assets/js/modernizr-custom.js
+++ _assets/js/modernizr-custom.js
@@ -1,5 +1,5 @@
 /*!
- * modernizr v3.7.1
+ * modernizr v3.4.0
  * Build https://modernizr.com/download?-bgsizecover-csscalc-flexbox-flexboxlegacy-flexboxtweener-srcset-setclasses-dontmin
  *
  * Copyright (c)
@@ -10,7 +10,6 @@
  *  Patrick Kettner
  *  Stu Cox
  *  Richard Herrera
- *  Veeck
 
  * MIT License
  */
@@ -24,19 +23,20 @@
 */
 
 ;(function(window, document, undefined){
-
   var tests = [];
   
 
   /**
+   *
    * ModernizrProto is the constructor for Modernizr
    *
    * @class
    * @access public
    */
+
   var ModernizrProto = {
     // The current version, dummy
-    _version: '3.7.1',
+    _version: '3.4.0',
 
     // Any settings that don't work as separate modules
     // can go in here as configuration.
@@ -95,20 +95,20 @@
    * @function is
    * @param {*} obj - A thing we want to check the type of
    * @param {string} type - A string to compare the typeof against
-   * @returns {boolean} true if the typeof the first parameter is exactly the specified type, false otherwise
+   * @returns {boolean}
    */
+
   function is(obj, type) {
     return typeof obj === type;
   }
-
   ;
 
   /**
    * Run through all tests and detect their support in the current UA.
    *
    * @access private
-   * @returns {void}
    */
+
   function testRunner() {
     var featureNames;
     var feature;
@@ -142,6 +142,7 @@
 
         // Run the test, or use the raw value if it's not a function
         result = is(feature.fn, 'function') ? feature.fn() : feature.fn;
+
 
         // Set each of the names on the Modernizr object
         for (nameIdx = 0; nameIdx < featureNames.length; nameIdx++) {
@@ -178,6 +179,7 @@
    * @access private
    * @returns {HTMLElement|SVGElement} The root element of the document
    */
+
   var docElement = document.documentElement;
   
 
@@ -187,8 +189,8 @@
    * @access private
    * @returns {boolean}
    */
-  var isSVG = docElement.nodeName.toLowerCase() === 'svg';
 
+  var isSVG = docElement.nodeName.toLowerCase() === 'svg';
   
 
   /**
@@ -198,6 +200,7 @@
    * @function setClasses
    * @param {string[]} classes - Array of class names
    */
+
   // Pass in an and array of class names, e.g.:
   //  ['no-webp', 'borderradius', ...]
   function setClasses(classes) {
@@ -217,15 +220,14 @@
 
     if (Modernizr._config.enableClasses) {
       // Add the new classes
-      if (classes.length > 0) {
-        className += ' ' + classPrefix + classes.join(' ' + classPrefix);
-      }
+      className += ' ' + classPrefix + classes.join(' ' + classPrefix);
       if (isSVG) {
         docElement.className.baseVal = className;
       } else {
         docElement.className = className;
       }
     }
+
   }
 
   ;
@@ -245,12 +247,14 @@
    * @access private
    * @returns {string} The string representing the vendor-specific style properties
    */
+
   var omPrefixes = 'Moz O ms Webkit';
   
 
   var cssomPrefixes = (ModernizrProto._config.usePrefixes ? omPrefixes.split(' ') : []);
   ModernizrProto._cssomPrefixes = cssomPrefixes;
   
+
 
   /**
    * contains checks to see if a string contains another string
@@ -259,8 +263,9 @@
    * @function contains
    * @param {string} str - The string we want to check for substrings
    * @param {string} substr - The substring we want to search the first string for
-   * @returns {boolean} true if and only if the first string 'str' contains the second string 'substr'
+   * @returns {boolean}
    */
+
   function contains(str, substr) {
     return !!~('' + str).indexOf(substr);
   }
@@ -277,6 +282,7 @@
    * @function createElement
    * @returns {HTMLElement|SVGElement} An HTML or SVG element
    */
+
   function createElement() {
     if (typeof document.createElement !== 'function') {
       // This is the case in IE7, where the type of createElement is "object".
@@ -296,6 +302,7 @@
    *
    * @access private
    */
+
   var modElem = {
     elem: createElement('modernizr')
   };
@@ -328,6 +335,7 @@
    * @returns {HTMLElement|SVGElement} Returns the real body of a document, or an
    * artificially created element that stands in for the body
    */
+
   function getBody() {
     // After page load injecting a fake body doesn't work so check if body exists
     var body = document.body;
@@ -349,11 +357,12 @@
    * @access private
    * @function injectElementWithStyles
    * @param {string} rule - String representing a css rule
-   * @param {Function} callback - A function that is used to test the injected element
+   * @param {function} callback - A function that is used to test the injected element
    * @param {number} [nodes] - An integer representing the number of additional nodes you want injected
    * @param {string[]} [testnames] - An array of strings that are used as ids for the additional nodes
-   * @returns {boolean} the result of the specified callback test
+   * @returns {boolean}
    */
+
   function injectElementWithStyles(rule, callback, nodes, testnames) {
     var mod = 'modernizr';
     var style;
@@ -412,6 +421,7 @@
     }
 
     return !!ret;
+
   }
 
   ;
@@ -425,12 +435,12 @@
    * @param {string} name - String name of camelCase prop we want to convert
    * @returns {string} The kebab-case version of the supplied name
    */
+
   function domToCSS(name) {
     return name.replace(/([A-Z])/g, function(str, m1) {
       return '-' + m1.toLowerCase();
     }).replace(/^ms-/, '-ms-');
   }
-
   ;
 
 
@@ -440,11 +450,11 @@
    *
    * @access private
    * @function computedStyle
-   * @param {HTMLElement|SVGElement} elem - The element we want to find the computed styles of
-   * @param {string|null} [pseudo] - An optional pseudo element selector (e.g. :before), of null if none
-   * @param {string} prop - A CSS property
-   * @returns {CSSStyleDeclaration} the value of the specified CSS property
+   * @param {HTMLElement|SVGElement} - The element we want to find the computed styles of
+   * @param {string|null} [pseudoSelector]- An optional pseudo element selector (e.g. :before), of null if none
+   * @returns {CSSStyleDeclaration}
    */
+
   function computedStyle(elem, pseudo, prop) {
     var result;
 
@@ -481,11 +491,12 @@
    * @param {string} value - A string representing the value we want to check via @supports
    * @returns {boolean|undefined} A boolean when @supports exists, undefined otherwise
    */
+
   // Accepts a list of property names and a single value
   // Returns `undefined` if native detection not available
   function nativeTestProps(props, value) {
     var i = props.length;
-    // Start with the JS API: https://www.w3.org/TR/css3-conditional/#the-css-interface
+    // Start with the JS API: http://www.w3.org/TR/css3-conditional/#the-css-interface
     if ('CSS' in window && 'supports' in window.CSS) {
       // Try every prefixed variant of the property
       while (i--) {
@@ -504,7 +515,7 @@
       }
       conditionText = conditionText.join(' or ');
       return injectElementWithStyles('@supports (' + conditionText + ') { #modernizr { position: absolute; } }', function(node) {
-        return computedStyle(node, null, 'position') === 'absolute';
+        return computedStyle(node, null, 'position') == 'absolute';
       });
     }
     return undefined;
@@ -520,12 +531,12 @@
    * @param {string} name - String name of kebab-case prop we want to convert
    * @returns {string} The camelCase version of the supplied name
    */
+
   function cssToDOM(name) {
     return name.replace(/([a-z])-([a-z])/g, function(str, m1, m2) {
       return m1 + m2.toUpperCase();
     }).replace(/^-/, '');
   }
-
   ;
 
   // testProps is a generic CSS / DOM property test.
@@ -603,16 +614,16 @@
           // supported. If `value` is empty string, it'll fail here (because
           // it hasn't changed), which matches how browsers have implemented
           // CSS.supports()
-          if (mStyle.style[prop] !== before) {
+          if (mStyle.style[prop] != before) {
             cleanElems();
-            return prefixed === 'pfx' ? prop : true;
+            return prefixed == 'pfx' ? prop : true;
           }
         }
         // Otherwise just return true, or the property name if this is a
         // `prefixed()` call
         else {
           cleanElems();
-          return prefixed === 'pfx' ? prop : true;
+          return prefixed == 'pfx' ? prop : true;
         }
       }
     }
@@ -625,7 +636,7 @@
   /**
    * List of JavaScript DOM values used for tests
    *
-   * @memberOf Modernizr
+   * @memberof Modernizr
    * @name Modernizr._domPrefixes
    * @optionName Modernizr._domPrefixes
    * @optionProp domPrefixes
@@ -639,6 +650,7 @@
    * Modernizr._domPrefixes === [ "Moz", "O", "ms", "Webkit" ];
    * ```
    */
+
   var domPrefixes = (ModernizrProto._config.usePrefixes ? omPrefixes.toLowerCase().split(' ') : []);
   ModernizrProto._domPrefixes = domPrefixes;
   
@@ -648,10 +660,11 @@
    *
    * @access private
    * @function fnBind
-   * @param {Function} fn - a function you want to change `this` reference to
-   * @param {Object} that - the `this` you want to call the function with
-   * @returns {Function} The wrapped version of the supplied function
+   * @param {function} fn - a function you want to change `this` reference to
+   * @param {object} that - the `this` you want to call the function with
+   * @returns {function} The wrapped version of the supplied function
    */
+
   function fnBind(fn, that) {
     return function() {
       return fn.apply(that, arguments);
@@ -666,9 +679,9 @@
    *
    * @access private
    * @function testDOMProps
-   * @param {Array<string>} props - An array of properties to test for
-   * @param {Object} obj - An object or Element you want to use to test the parameters again
-   * @param {boolean|Object} elem - An Element to bind the property lookup again. Use `false` to prevent the check
+   * @param {array.<string>} props - An array of properties to test for
+   * @param {object} obj - An object or Element you want to use to test the parameters again
+   * @param {boolean|object} elem - An Element to bind the property lookup again. Use `false` to prevent the check
    * @returns {false|*} returns false if the prop is unsupported, otherwise the value that is supported
    */
   function testDOMProps(props, obj, elem) {
@@ -686,7 +699,7 @@
 
         // let's bind a function
         if (is(item, 'function')) {
-          // bind to obj unless overridden
+          // bind to obj unless overriden
           return fnBind(item, elem || obj);
         }
 
@@ -708,7 +721,7 @@
    * @access private
    * @function testPropsAll
    * @param {string} prop - A string of the property to test for
-   * @param {string|Object} [prefixed] - An object to check the prefixed properties on. Use a string to skip
+   * @param {string|object} [prefixed] - An object to check the prefixed properties on. Use a string to skip
    * @param {HTMLElement|SVGElement} [elem] - An element used to test the property and value against
    * @param {string} [value] - A string of a css value
    * @param {boolean} [skipValueTest] - An boolean representing if you want to test if value sticks when set
@@ -742,7 +755,7 @@
   /**
    * testAllProps determines whether a given CSS property is supported in the browser
    *
-   * @memberOf Modernizr
+   * @memberof Modernizr
    * @name Modernizr.testAllProps
    * @optionName Modernizr.testAllProps()
    * @optionProp testAllProps
@@ -751,7 +764,6 @@
    * @param {string} prop - String naming the property to test (either camelCase or kebab-case)
    * @param {string} [value] - String of the value to test
    * @param {boolean} [skipValueTest=false] - Whether to skip testing that the value is supported when using non-native detection
-   * @returns {false|string} returns the string version of the property, or false if it is unsupported
    * @example
    *
    * testAllProps determines whether a given CSS property, in some prefixed form,
@@ -776,12 +788,11 @@
    * testAllProps('shapeOutside', 'content-box', true);
    * ```
    */
+
   function testAllProps(prop, value, skipValueTest) {
     return testPropsAll(prop, undefined, undefined, value, skipValueTest);
   }
-
   ModernizrProto.testAllProps = testAllProps;
-
   
 /*!
 {
@@ -790,7 +801,7 @@
   "tags": ["css"],
   "builderAliases": ["css_backgroundsizecover"],
   "notes": [{
-    "name": "MDN Docs",
+    "name" : "MDN Docs",
     "href": "https://developer.mozilla.org/en/CSS/background-size"
   }]
 }
@@ -802,9 +813,9 @@
 
   /**
    * List of property values to set for css tests. See ticket #21
-   * https://github.com/modernizr/modernizr/issues/21
+   * http://git.io/vUGl4
    *
-   * @memberOf Modernizr
+   * @memberof Modernizr
    * @name Modernizr._prefixes
    * @optionName Modernizr._prefixes
    * @optionProp prefixes
@@ -831,6 +842,7 @@
    * rule === 'display:flex; display:-webkit-flex; display:-moz-flex; display:-o-flex; display:-ms-flex; display:flex'
    * ```
    */
+
   // we use ['',''] rather than an empty array in order to allow a pattern of .`join()`ing prefixes to test
   // values in feature detects to continue to work
   var prefixes = (ModernizrProto._config.usePrefixes ? ' -webkit- -moz- -o- -ms- '.split(' ') : ['','']);
@@ -877,7 +889,7 @@ Method of allowing calculated values for length units. For example:
   "tags": ["css"],
   "notes": [{
     "name": "The _new_ flexbox",
-    "href": "https://www.w3.org/TR/css-flexbox-1/"
+    "href": "http://dev.w3.org/csswg/css3-flexbox"
   }],
   "warnings": [
     "A `true` result for this detect does not imply that the `flex-wrap` property is supported; see the `flexwrap` detect."
@@ -925,15 +937,14 @@ Detects support for the Flexible Box Layout model, a.k.a. Flexbox, which allows 
 {
   "name": "srcset attribute",
   "property": "srcset",
-  "caniuse": "srcset",
   "tags": ["image"],
   "notes": [{
     "name": "Smashing Magazine Article",
-    "href": "https://www.smashingmagazine.com/2013/08/webkit-implements-srcset-and-why-its-a-good-thing/"
-  },{
+    "href": "https://en.wikipedia.org/wiki/APNG"
+    },{
     "name": "Generate multi-resolution images for srcset with Grunt",
     "href": "https://addyosmani.com/blog/generate-multi-resolution-images-for-srcset-with-grunt/"
-  }]
+    }]
 }
 !*/
 /* DOC
